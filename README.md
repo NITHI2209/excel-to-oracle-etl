96ceb3ca05733f6a0697e2e9ef7906d70569cdd2
NITHYA SREE S <nithyasree065@gmail.com>
2026-06-18 12:43:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+Excel to Oracle ETL Project
+
+Overview
+
+This project reads student data from an Excel file using Python and loads it into an Oracle Database table.
+
+Technologies Used
+
+* Python
+* Pandas
+* Oracle Database 21c XE
+* VS Code
+
+Workflow
+
+Excel File → Python → Oracle Database
+
+Columns
+
+* NAME
+* FATHER_NAME
+* DOB
+* QUALIFICATION
+
+Output
+
+Successfully inserts Excel data into the Oracle STUDENT table.
---
+++ project1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import oracledb
+
+df = pd.read_excel("students.xlsx")
+
+conn = oracledb.connect(
+    user="sys",
+    password="Nithyasree@22",
+    dsn="localhost:1521/XEPDB1",
+    mode=oracledb.AUTH_MODE_SYSDBA
+)
+
+cursor = conn.cursor()
+
+for index, row in df.iterrows():
+    cursor.execute(
+        """
+        INSERT INTO STUDENT
+        (NAME, FATHER_NAME, DOB, QUALIFICATION)
+        VALUES (:1, :2, :3, :4)
+        """,
+        (
+            row["NAME"],
+            row["FATHER_NAME"],
+            row["DOB"],
+            row["QUALIFICATION"]
+        )
+    )
+
+conn.commit()
+
+print("Data Inserted Successfully!")
+
+cursor.close()
+conn.close()
